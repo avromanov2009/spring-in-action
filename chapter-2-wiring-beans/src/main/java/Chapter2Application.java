1ab728add2703cce1da2817b18d23050da837aee
Ross Moug <ross.moug@gmail.com>
2018-06-07 21:41:21 +0100

    Chapter 2 basic module setup.

---
+++ chapter-2-wiring-beans/src/main/java/Chapter2Application.java
@@ -0,0 +1,18 @@
+import config.Config;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import static org.springframework.boot.SpringApplication.exit;
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+@ComponentScan(basePackageClasses = Config.class)
+public class Chapter2Application {
+    public static void main(String[] args) {
+        run(Chapter2Application.class, args);
+        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+
+        System.exit(exit(context, () -> 0));
+    }
+}
