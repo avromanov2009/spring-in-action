d7545d4c354b5a2e096ceb4c0d832ef73e95d82c
Ross Moug <ross.moug@gmail.com>
2018-06-08 20:51:47 +0100

    Chapter 2 - wiring up beans using Spring.

--- chapter-2-wiring-beans/src/main/java/Chapter2Application.java
+++ chapter-2-wiring-beans/src/main/java/Chapter2Application.java
@@ -1,4 +1,4 @@
-import config.Config;
+import soundsystem.CDPlayerConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,11 +7,11 @@ import static org.springframework.boot.SpringApplication.exit;
 import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = Config.class)
+@ComponentScan(basePackageClasses = CDPlayerConfig.class)
 public class Chapter2Application {
     public static void main(String[] args) {
         run(Chapter2Application.class, args);
-        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CDPlayerConfig.class);
 
         System.exit(exit(context, () -> 0));
     }
---
+++ chapter-2-wiring-beans/src/test/java/soundsystem/CDPlayerTest.java
@@ -0,0 +1,38 @@
+package soundsystem;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = CDPlayerConfig.class)
+public class CDPlayerTest {
+    @Rule
+    public final SystemOutRule log = new SystemOutRule().enableLog();
+
+    @Autowired
+    private MediaPlayer mediaPlayer;
+
+    @Autowired
+    private CompactDisc cd;
+
+    @Test
+    public void cdShouldNotBeNull() {
+        assertNotNull(cd);
+    }
+
+    @Test
+    public void play() {
+        log.clearLog();
+        mediaPlayer.play();
+        assertEquals("Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\n", log.getLog());
+    }
+}
